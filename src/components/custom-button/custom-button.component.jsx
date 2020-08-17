5c12e3c8f15513c9a5b604ed77ea3fafc52c0436
CosminFlorin <mihalceacosminflorin@hotmail.com>
2020-08-17 18:43:02 +0300

    added redux

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -6,9 +6,10 @@ const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
   <button
     className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
     {...otherProps}
+    type="button"
   >
     {children}
   </button>
 );
 
-export default CustomButton;
+export default CustomButton;  
